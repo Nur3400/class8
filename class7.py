895aaf48bed0f569b8b68f0d4440898ba1cafd61
“Nur3400” <mathnurislam@gmail.com>
2021-05-05 13:54:08 +0600

    class7

---
+++ class7.py
@@ -0,0 +1,38 @@
+ab=("I like to play guiter and i am huge fan of sungha jung and he play very good guiter")
+xy=(ab.find("play"))
+tuv=(ab.find("play",xy+1))
+print(tuv)
+
+
+
+ab=("I like to play guiter and i am huge fan of sungha jung and he play very good guiter")
+xy=(ab.find("play"))
+tuv=(ab.find("play",xy+1))
+print(tuv)
+print(ab[tuv:])
+
+
+
+
+abc=("I like to play guiter and i am huge fan of sungha jung and he play very good guiter")
+xyz=(abc.find("play"))
+tuv=(abc.find("play",xyz+1))
+print(tuv)
+
+abc=("I like to play guiter and i am huge fan of sungha jung and he play very good guiter")
+xyz=(abc.find("play"))
+tuv=(abc.find("play",xyz+1))
+print(tuv)
+print(abc[tuv:])
+
+#requesting input
+print("Hello")
+x=input("what is your name ")
+print("hello nice to meet you "+ x)
+
+#requesting input
+print("Hello")
+x=input("what is your name ")
+print(x+" lenght of your name is " + str(len(x)))
+y=input("what is your age: ")
+print(("you will be ")+str(int(y)+1)+(" in a years"))
